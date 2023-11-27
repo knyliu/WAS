e34246175512a45de79a053d99c60f9e4ddc0d55
knyliu <131148428+knyliu@users.noreply.github.com>
2023-11-27 10:41:27 +0800

    DB_finalproject_client_v1.0

---
+++ DB_finalproject_client/client/src/pages/saved-appointments.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+
+export const SavedAppointments = () => {
+  const [yourAppointments, setYourAppointments] = useState([]); 
+  const userID = useGetUserID();
+
+  console.log("yourAppointments");
+  console.log(yourAppointments);
+
+
+  useEffect(() => {
+    
+
+
+    const fetchYourAppointments = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/appointments/client_appointments");
+    
+        const filteredAppointments = response.data.filter(appointment => 
+          appointment.appointmentInputs.userOwner === userID);
+        setYourAppointments(filteredAppointments);
+        
+        // console.log("Filtered appointments:", filteredAppointments);
+        // console.log(yourAppointments);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    
+    fetchYourAppointments();
+    }, [userID]);
+    
+
+
+
+
+    return (
+      <div>
+        <h1>Your Appointments</h1>
+        <ul>
+          {yourAppointments.map((appointment) => (
+            <li key={appointment._id}>
+              <div>
+                <h2>{appointment._id}</h2>
+                <table>
+                  <tbody>
+                    {Object.entries(appointment.appointmentInputs).map(([key, value]) => (
+                      <tr key={key}>
+                        <td>{key}</td>
+                        <td>{value}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+    
+};
